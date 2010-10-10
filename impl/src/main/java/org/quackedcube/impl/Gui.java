87df255c9db649c5b3771ae89ebb3e3b5132f629
Lord.Quackstar <Lord.Quackstar@gmail.com>
2010-10-10 02:09:41 +0000

    Clock works now (can be initiated with clicks). Just needs outside control

--- impl/src/main/java/org/quackedcube/impl/Gui.java
+++ impl/src/main/java/org/quackedcube/impl/Gui.java
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with QuackedCube.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.quackedcube.impl;
 
 import java.awt.BorderLayout;
@@ -25,6 +24,8 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -34,6 +35,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.JTextPane;
+import javax.swing.Timer;
+import org.apache.commons.lang.time.DurationFormatUtils;
+import org.apache.commons.lang.time.StopWatch;
 import org.quackedcube.virtualcube.VirtualBuilder;
 
 /**
@@ -41,7 +45,7 @@ import org.quackedcube.virtualcube.VirtualBuilder;
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
 public class Gui extends JFrame {
-	JLabel clock;
+	Clock clock;
 	VirtualBuilder virtualCube;
 	Dimension frameSize = new Dimension(800, 800);
 
@@ -68,13 +72,15 @@ public class Gui extends JFrame {
 		logPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Log"));
 
 		//Clock setup
-		clock = new JLabel("00:00.00", JLabel.CENTER);
+		clock = new Clock();
 		//clock.setForeground(Color.yellow);
 		clock.setPreferredSize(new Dimension((int) clock.getPreferredSize().getWidth(), 100));
 		clock.setFont(new Font("Arial", Font.PLAIN, 180));
 		clock.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Timer"));
 		clock.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 		clock.setAlignmentY(JComponent.CENTER_ALIGNMENT);
+
+
 		clockPanel.add(clock, BorderLayout.CENTER);
 
 		//Logging panel setup
@@ -108,17 +114,88 @@ public class Gui extends JFrame {
 			}
 		});
 		/*virtualCubeControl.add(new JButton("Reset Position") {{
-			addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						Gui.this.virtualCube.resetPosition();
-					}
-				});
+		addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		Gui.this.virtualCube.resetPosition();
+		}
+		});
 		}
 		});*/
 
 		virtualCubePanel.add(virtualCubeControl, BorderLayout.SOUTH);
 
 		return horozontalSplit;
+	}
+
+	public class Clock extends JLabel implements MouseListener {
+		boolean on = false;
+		boolean watchRan = false;
+		Timer timer = new Timer(10, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				StopWatch watch = Clock.this.watch;
+				watch.split();
+				Clock.this.setText(Clock.this.format(watch.getSplitTime()));
+				Clock.this.validate();
+				Clock.this.repaint();
+				watch.unsplit();
+			}
+		});
+		StopWatch watch = new StopWatch();
+
+		public Clock() {
+			super("", JLabel.CENTER);
+			setText(format(watch.getTime()));
+			addMouseListener(this);
+			timer.setCoalesce(true);
+		}
+
+		protected String format(long time) {
+			return DurationFormatUtils.formatDuration(time, "mm:ss.SSS", true);
+		}
+
+		public void start() {
+			on = true;
+			timer.start();
+			if (!watchRan)
+				watch.start();
+			else
+				watch.resume();
+			System.out.println("Starting...");
+		}
+
+		public void stop() {
+			on = false;
+			watchRan = true;
+			watch.suspend();
+			timer.stop();
+			System.out.println("Stopping");
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (on)
+				stop();
+			else
+				start();
+			System.out.println("Recieved Clicked");
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+		}
 	}
 }
